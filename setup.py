20b3a5e3c22e4754fc4423b3bea1565ab056533d
Cory <corypruce@gmail.com>
2015-10-25 21:31:17 -0700

    added classifier and mysql configs, condensed. adding setup

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+from setuptools import setup, find_packages
+
+
+
+def try_build():
+    print 'Build started'
+    build_ok = True
+
+
+    try:
+        setup(
+            name = 'botanist',
+            version = '1.0',
+            packages = find_packages(),
+            scripts = ['tagger.py', 'classifier.py', ],
+            install_requires=['pydot', 'MySQLdb', 'androguard',],
+        )
+    except SystemExit, e:
+        print repr(e)
+        build_ok = False
+
+    if build_ok == True:
+        print 'Build success'
+    else:
+        print 'Build failed'
+
+
+try_build()
