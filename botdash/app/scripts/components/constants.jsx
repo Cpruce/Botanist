04580e0cae1894ec71f811916ac9c738dc833a2d
Cory <corypruce@gmail.com>
2015-11-30 11:04:23 -0800

    removing some frontends

--- botdash/app/scripts/components/constants.jsx
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    changed: "changed",
-    all: "all",
-    create: "create",
-    update: "update",
-    remove: "remove"
-};
--- botdash/app/scripts/constants/index.jsx
+++
@@ -1,7 +0,0 @@
-"use strict";
-
-module.exports = {
-    constants: require("./.constants.jsx")
-};
-
-
