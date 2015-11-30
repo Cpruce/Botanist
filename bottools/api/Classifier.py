b1b51f4fec5d9509ba4882c4e282c392258fcd80
Kumar Vikramjeet <vik001ind@gmail.com>
2015-11-30 12:41:19 -0800

    classifer fix

--- bottools/api/Classifier.py
+++ bottools/api/Classifier.py
@@ -14,17 +14,19 @@
 
 # Tapered Levenshtein for mnemonic signatures
 def tapered_levenshtein(sig1, sig2):
-    sig1_mnemonics = sig1.split()
-    sig2_mnemonics = sig2.split()
+    sig1_mnemonics = sig1
+    sig2_mnemonics = sig2
     position = 0
-    num_mnemonics = len(sig1_mnemonics) if len(sig1_mnemonics)
-                    < len(sig2_mnemonics) else len(sig2_mnemonics)
+    if len(sig1_mnemonics) < len(sig2_mnemonics):
+        num_mnemonics = len(sig1_mnemonics)  
+    else: 
+        num_mnemonics = len(sig2_mnemonics)
                     # assumption len(sig1) == len(sig2)
 
     weight = 1.0 - position/num_mnemonics
     distance = 0.0
 
-    for mn1, mn2 in sig1_mnemonics, sig2_mnemonics:
+    for mn1, mn2 in zip(sig1_mnemonics, sig2_mnemonics):
         if mn1 != mn2:
             distance+=weight
 
